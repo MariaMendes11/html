1268ae454d5a3345c191c464a5fa483138d4e3cd
MariaMendes11 <mcmendes900@gmail.com>
2024-08-28 14:15:30 -0300

    atividade

--- API/server.js
+++
@@ -1,5 +0,0 @@
-//Importar a instancia do Express configurada em index.js
-const app = require("./index");
-
-//Inicia o servidor na porta 3000, nesta caso a API sera acessivel em http://localhost:3000
-app.listen(3000);
---
+++ API/src/server.js
@@ -0,0 +1,5 @@
+//Importar a instancia do Express configurada em index.js
+const app = require("./index");
+
+//Inicia o servidor na porta 3000, nesta caso a API sera acessivel em http://localhost:5000
+app.listen(5000);
